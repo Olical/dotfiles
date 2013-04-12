1dbbd27cc2234d18b8c830f50476eedd5432b82d
Oliver Caldwell <ocaldwel@amazon.com>
2013-04-12 17:33:58 +0100

    Updated the sync-bundles.sh script.
    It now installs Vundle if it isn't found and will give more information
    as it goes.

--- sync-bundles.sh
+++ sync-bundles.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+echo "Checking for Vundle..."
+if [ ! -f ~/.vim/bundle/vundle/README.md ];
+then
+	echo "Vundle not found, installing..."
+	mkdir -p ~/.vim/bundle
+	git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
+else
+	echo "Vundle found."
+fi
+
+echo "Removing, updating and installing bundles..."
 vim +BundleClean! +BundleInstall! +qall
 echo "All synced!"
