0ff39dc1b266dd6d65088da3dd6a78c266cc20a5
Oliver Caldwell <ocaldwel@amazon.com>
2013-04-25 10:29:49 +0100

    Vim will only be compiled if the configure works.

--- compile-vim.sh
+++ compile-vim.sh
@@ -4,5 +4,4 @@ mkdir builds
 cd builds
 hg clone http://hg.debian.org/hg/pkg-vim/vim
 cd vim
-./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp
-make
+./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp && make
