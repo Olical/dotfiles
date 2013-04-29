f21fb9259a7ec7c3d7160e29558726ec8b1ec0f2
Oliver Caldwell <ocaldwel@amazon.com>
2013-04-29 11:53:39 +0100

    Updated bash tweaks and added screen.
    So now screen colours work correctly! OH MY LAWDY! You just need to
    source the new screen tweaks file. Also added a ftdetect for screen.
    Looks for *.screen.

--- bash-tweaks.sh
+++ bash-tweaks.sh
@@ -1,2 +1,3 @@
+export TERM=xterm-256color
 export PS1="\[\e[0;32m\]\W\[\e[0;31m\]\$\[\e[0m\] "
 echo $'\e[00;31mDON\'T PANIC\e[00m'
---
+++ ftdetect/screen.vim
@@ -0,0 +1,1 @@
+au BufNewFile,BufRead *.screen set filetype=screen
