84755eef3965d72202133e0e0b152851e897c2d0
Oliver Caldwell <ocaldwel@amazon.com>
2013-05-01 11:35:40 +0100

    Setup bash / tmux powerline config.
    Also improved the tmux config.

--- bash-tweaks.sh
+++ bash-tweaks.sh
@@ -1,3 +1,8 @@
+PATH=$PATH:~/.vim/bundle/powerline/scripts
+export PATH
+
+source ~/.vim/bundle/powerline/powerline/bindings/bash/powerline.sh
+
 export TERM=xterm-256color
 export PS1="\[\e[0;32m\]\W\[\e[0;31m\]\$\[\e[0m\] "
 echo $'\e[00;31mDON\'T PANIC\e[00m'
