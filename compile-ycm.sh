b78488cb8b5333e5ab5c45e449abc0926bbf102e
Oliver Caldwell <ocaldwel@amazon.com>
2013-05-29 11:09:49 +0100

    Upgraded the YCM build script.

--- compile-ycm.sh
+++ compile-ycm.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-pushd ~
-rm -rf ycm_build
-mkdir ycm_build
-cd ycm_build
 sudo apt-get install python-dev
-cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/cpp
-make ycm_core
-popd
+cd ./bundle/YouCompleteMe/
+./install.sh
