c4798bb431a1e1a12b93721e9982bc2e573b5159
Oliver Caldwell <olliec87@gmail.com>
2013-06-15 10:35:30 +0100

    Added another vim compile option.

--- compile-vim.sh
+++ compile-vim.sh
@@ -18,4 +18,4 @@ else
 	hg update
 fi
 
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev && ./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp && make
+sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev && ./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-cscope && make
