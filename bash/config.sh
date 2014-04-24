73ddb26d5497f89945b1838f84c0066b99adb9e7
Oliver Caldwell <oliver.caldwell@lab49.com>
2014-04-24 15:40:08 +0100

    Fixed bash prompt include path.

--- bash/config.sh
+++ bash/config.sh
@@ -1,5 +1,7 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 alias ls='ls -G'
 export LSCOLORS='ExGxBxDxCxEgEdxbxgxcxd'
 export EDITOR='vim'
 
-source ./prompt.sh
+source $DIR/prompt.sh
