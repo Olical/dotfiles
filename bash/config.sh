6918fcb38372d02db7961dca7efcd55a465e9799
Oliver Caldwell <oliver.caldwell@lab49.com>
2014-05-20 11:45:42 +0100

    Alias vi to a vim suggestion.

--- bash/config.sh
+++ bash/config.sh
@@ -2,6 +2,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 alias ls='ls --color'
 export LSCOLORS='ExGxBxDxCxEgEdxbxgxcxd'
+
+alias vi='echo "I think you mean vim..."'
 export EDITOR='vim'
 
 source $DIR/prompt.sh
