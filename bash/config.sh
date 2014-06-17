4f9a2450043025a1c4d8dc2c2c5f68c2809819f8
Oliver Caldwell <oliver.caldwell@lab49.com>
2014-06-17 10:23:06 +0100

    Prepended the status alias with a g.

--- bash/config.sh
+++ bash/config.sh
@@ -6,6 +6,6 @@ export LSCOLORS='ExGxBxDxCxEgEdxbxgxcxd'
 alias vi='echo "I think you mean vim..."'
 export EDITOR='vim'
 
-alias s='git status --short --branch'
+alias gs='git status --short --branch'
 
 source $DIR/prompt.sh
