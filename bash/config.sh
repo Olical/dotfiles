23008fa340ea96abb89870555a6efd08b15e34fa
Oliver Caldwell <oliver.caldwell@lab49.com>
2014-06-17 10:25:29 +0100

    Aliases for git-smart.

--- bash/config.sh
+++ bash/config.sh
@@ -7,5 +7,8 @@ alias vi='echo "I think you mean vim..."'
 export EDITOR='vim'
 
 alias gs='git status --short --branch'
+alias gup='git smart-pull'
+alias gm='git smart-merge'
+alias gl='git smart-log'
 
 source $DIR/prompt.sh
