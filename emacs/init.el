86004e61aa22fd6d78f8c5592f616344b5424024
Oliver Caldwell <olliec87@gmail.com>
2014-09-17 22:18:06 +0100

    Initial custom emacs configuration.
    I added some cool stuff to load files from the libraries directory which
    in turn install dependencies and tweak configuration of Emacs as a
    whole. So I have evil mode running, although at a very basic level.
    Slowly removing the crap I don't need, then I'll build up to the things
    that I do need.

--- emacs/init.el
+++ emacs/init.el
@@ -1,36 +1,11 @@
-(when (< emacs-major-version 24)
-  (error "This setup requires Emacs v24, or higher. You have: v%d" emacs-major-version))
+(defvar libraries '("packages"
+                    "tweaks"))
 
-(require 'package)
-(package-initialize)
+(defvar init-path (or load-file-name (buffer-file-name)))
+(defvar current-directory
+  (let ((segments (butlast (split-string init-path "/"))))
+    (mapconcat 'identity segments "/")))
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(defvar my-packages '(evil)
-  "A list of packages to check for and install at launch.")
-
-(defun my-missing-packages ()
-  (let (missing-packages)
-    (dolist (package my-packages (reverse missing-packages))
-      (or (package-installed-p package)
-      (push package missing-packages)))))
-
-(defun ensure-my-packages ()
-  (let ((missing (my-missing-packages)))
-    (when missing
-      ; Check for new packages (package versions)
-      (package-refresh-contents)
-      ; Install the missing packages
-      (mapc (lambda (package)
-          (when (not (package-installed-p package))
-        (package-install package)))
-        missing)
-      ; Close the compilation log.
-      (let ((compile-window (get-buffer-window "*Compile-Log*")))
-    (if compile-window
-        (delete-window compile-window))))))
-
-(ensure-my-packages)
-
-(evil-mode t)
+(defvar here )
+(add-to-list 'load-path (format "%s/%s" current-directory "libraries"))
+(mapc 'load-library libraries)
---
+++ emacs/libraries/packages.el
@@ -0,0 +1,24 @@
+(require 'package)
+(require 'cl)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(defvar packages '(evil))
+
+(defun packages-installed-p ()
+  (loop for p in packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (packages-installed-p)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  (dolist (p packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+
+(require 'evil)
+(evil-mode t)
