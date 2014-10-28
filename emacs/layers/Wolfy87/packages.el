691f8aa35ac7ace49c37b3d1981e3b45fee333cf
Oliver Caldwell <oliver.caldwell@lab49.com>
2014-10-28 11:19:33 +0000

    Removed unused evil-args bindings.
    Also removed progn where it wasn't needed.

--- emacs/layers/Wolfy87/packages.el
+++ emacs/layers/Wolfy87/packages.el
@@ -10,26 +10,15 @@
   (use-package evil-args
     :init
     (progn
-      ;; Bind evil-args text objects.
       (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
-      (define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
-
-      ;; Bind evil-forward/backward-args.
-      (define-key evil-normal-state-map "L" 'evil-forward-arg)
-      (define-key evil-normal-state-map "H" 'evil-backward-arg)
-      (define-key evil-motion-state-map "L" 'evil-forward-arg)
-      (define-key evil-motion-state-map "H" 'evil-backward-arg)
-
-      ;; Bind evil-jump-out-args.
-      (define-key evil-normal-state-map "K" 'evil-jump-out-args))))
+      (define-key evil-outer-text-objects-map "a" 'evil-outer-arg))))
 
 (defun Wolfy87/init-evil-numbers ()
   (use-package evil-numbers
     :init
-    (progn
-      (evil-leader/set-key
-        "=" 'evil-numbers/inc-at-pt
-        "-" 'evil-numbers/dec-at-pt))))
+    (evil-leader/set-key
+      "=" 'evil-numbers/inc-at-pt
+      "-" 'evil-numbers/dec-at-pt)))
 
 (defun Wolfy87/init-evil-search-highlight-persist ()
   (use-package evil-search-highlight-persist
@@ -42,7 +31,6 @@
 (defun Wolfy87/init-smooth-scrolling ()
   (use-package smooth-scrolling
     :init
-    (progn
-      (setq scroll-margin 5
-            scroll-conservatively 9999
-            scroll-step 1))))
+    (setq scroll-margin 5
+          scroll-conservatively 9999
+          scroll-step 1)))
