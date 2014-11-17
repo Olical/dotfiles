a09ff7b4d3be1b705f24e8ac5ef75b77d6867fb0
Oliver Caldwell <olliec87@gmail.com>
2014-11-17 10:38:35 +0000

    Vim install should create the undo directory.

--- vim/install.sh
+++ vim/install.sh
@@ -2,5 +2,5 @@
 
 ln -s ~/dotfiles/vim ~/.vim
 echo "source $HOME/.vim/bootstrap.vim" >> ~/.vimrc
-mkdir ~/.vim/bundle
+mkdir ~/.vim/bundle/ ~/.vim/undo/
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
