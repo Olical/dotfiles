f4d98edc201954893a92a4fe18ac9b0fb9ff945c
Oliver Caldwell <olliec87@gmail.com>
2014-11-21 08:57:05 +0000

    autoloads -> autoload

--- vim/install.sh
+++ vim/install.sh
@@ -2,5 +2,5 @@
 
 ln -s ~/dotfiles/vim ~/.vim
 echo "source $HOME/.vim/bootstrap.vim" >> ~/.vimrc
-mkdir ~/.vim/undo/ ~/.vim/spell ~/.vim/autoloads
+mkdir ~/.vim/undo/ ~/.vim/spell ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
