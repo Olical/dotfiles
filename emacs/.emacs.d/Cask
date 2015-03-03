7273fe86d90781b99365910f2fc4d3c22c4b4a61
Oliver Caldwell <olliec87@gmail.com>
2015-03-03 00:20:05 +0000

    I have packages syncing in Emacs!
    Need to fix some weird issue with loaddefs not keeping in sync and
    consider when I want the install / update / clean to run, but it works.
    Next step is modularisation too.

--- emacs/.emacs.d/Cask
+++
@@ -1,2 +0,0 @@
-(source gnu)
-(source melpa)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,2 +1,21 @@
-(require 'cask "~/.emacs.d/cask-repo/cask.el")
-(cask-initialize)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+    (url-retrieve-synchronously
+      "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+(setq dotfiles-required-packages '())
+(defmacro bundle (name &rest content)
+  `(progn
+    (add-to-list 'dotfiles-required-packages ',name)
+    (el-get-bundle ,name ,@content)))
+
+(bundle monokai-theme
+        (load-theme 'monokai t))
+
+(el-get-cleanup dotfiles-required-packages)
