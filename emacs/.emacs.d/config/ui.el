20e8c508ffba42c486f0fe377255a18c47f1eb80
Oliver Caldwell <oliver.caldwell@lab49.com>
2015-03-04 10:00:58 +0000

    Improved Emacs maximise / full screen on OSX

--- emacs/.emacs.d/config/ui.el
+++ emacs/.emacs.d/config/ui.el
@@ -3,3 +3,9 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+;; Maximise by default.
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Don't use the awful OSX full screen.
+(setq ns-use-native-fullscreen nil)
