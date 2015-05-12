eaa049b56835ce43a0586067033a1ccaed500d2a
Oliver Caldwell <olliec87@gmail.com>
2015-05-12 13:46:16 +0100

    Removed i3 lock image
    Now just blurring my desktop.

--- i3/.i3/sync.sh
+++ i3/.i3/sync.sh
@@ -9,4 +9,3 @@ fi
 mkdir $BGS
 
 curl http://oli.me.uk/wp-content/uploads/2015/03/very-angular.png -o $BGS/desktop.png
-curl http://oli.me.uk/wp-content/uploads/2015/03/dark-sharp-edges.png -o $BGS/lock.png
