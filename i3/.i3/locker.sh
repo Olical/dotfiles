90871fbc2b6d0ca36d98a7ec1ed5357dcaf49717
Oliver Caldwell <olliec87@gmail.com>
2015-06-19 13:59:29 +0100

    Now using i3lock-wrapper if available
    It blurs my desktop and uses that as a lock screen.

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-i3lock -i ~/.i3/backgrounds/lock.png --tiling && sleep 1
+if ! type "i3lock-wrapper" > /dev/null; then
+    i3lock -i ~/.i3/backgrounds/lock.png --tiling && sleep 1
+else
+    i3lock-wrapper && sleep 1
+fi
