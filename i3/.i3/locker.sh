7a286304d836d52a48af293df3c9c86c283a87d0
Oliver Caldwell <olliec87@gmail.com>
2015-06-19 14:12:04 +0100

    Just using i3lock-wrapper now

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if ! type "i3lock-wrapper" > /dev/null; then
-    i3lock -i ~/.i3/backgrounds/lock.png --tiling && sleep 1
-else
-    i3lock-wrapper && sleep 1
-fi
+i3lock-wrapper && sleep 1
