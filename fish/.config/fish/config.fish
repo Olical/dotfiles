97101f0e0e034a81ba559210411a1fc2726ff39c
Oliver Caldwell <ollie@qubit.com>
2015-08-25 11:14:41 +0100

    Move git alias to git config works with completion

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,22 +7,9 @@ alias dockerkill='docker kill (docker ps -q)'
 # Delete all docker images and containers.
 alias dockernuke='docker rm (docker ps -a -q); docker rmi -f (docker images -q)'
 
-# Git.
+# Git alias.
 alias g="git"
-alias gs="git status -sb"
-alias gb="git branch"
-alias gc="git commit -v"
-alias ga="git add"
-alias gco="git checkout"
-alias gl="git pull"
-alias gp="git push"
-alias gf="git fetch"
-alias gmt="git mergetool"
-alias gm="git merge"
-alias gr="git remote"
-alias gre="git rebase"
-alias glg="git log --stat"
-alias gd="git diff"
+alias gs="echo WooOoOooOoOooOo~"
 
 # Start an SSH agent if required, if not, connect to it.
 initialise_ssh_agent
