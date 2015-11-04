d1472501f134278216c1481fca3800ec4c5822c7
Oliver Caldwell <ollie@qubit.com>
2015-11-04 10:01:44 +0000

    Actually exit locker with 0 if already running

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pgrep i3lock && exit 1
+pgrep i3lock && exit 0
 
 i3lock -i ~/.i3/backgrounds/congruent_outline.png --tiling && sleep 1
 sleep 60; pgrep i3lock && xset dpms force off
