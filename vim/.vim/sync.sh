e3455757a9c2c19ca1ea12d1058d5505935d0873
Oliver Caldwell <ollie@qubit.com>
2016-01-05 10:12:05 +0000

    Use snapshot.vim, not revert.sh

--- vim/.vim/sync.sh
+++ vim/.vim/sync.sh
@@ -6,4 +6,4 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-vim +"PlugSnapshot $HOME/.vim/revert.sh" +PlugUpgrade +PlugClean! +PlugUpdate +qa
+vim +"PlugSnapshot! $HOME/.vim/snapshot.vim" +PlugUpgrade +PlugClean! +PlugUpdate +qa
