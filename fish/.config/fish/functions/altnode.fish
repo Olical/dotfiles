b9153209ea358201ba62de7be9aa56bc1e34f489
Oliver Caldwell <ollie@qubit.com>
2016-02-29 16:15:45 +0000

    Small error handling additions

--- fish/.config/fish/functions/altnode.fish
+++ fish/.config/fish/functions/altnode.fish
@@ -10,17 +10,28 @@ function altnode
       case local
         ls ~/.altnode/cache | cut -d "." -f 1-3
       case remote
-        curl "$source/" | grep "v[0-9\\.]*/" | cut -d "\"" -f 2 | cut -d "/" -f 1
-      case clear
+        curl -s "$source/" | grep "v[0-9\\.]*/" | cut -d "\"" -f 2 | cut -d "/" -f 1
+      case reset
         rm ~/bin/node
       case fetch
         set -l ver $argv[2]
-        curl "$source/$ver/node-$ver-linux-x86.tar.gz" -o "$HOME/.altnode/cache/$ver.tar.gz"
+        curl -f "$source/$ver/node-$ver-linux-x86.tar.gz" -o "$HOME/.altnode/cache/$ver.tar.gz"
+
+        if [ $status != "0" ]
+          echo "Node version '$ver' not found on the remote"
+        end
       case use
         set -l ver $argv[2]
-        tar -zxvf "$HOME/.altnode/cache/$ver.tar.gz" -C "/tmp/altnode" "node-$ver-linux-x86/bin/node"
-        mv "/tmp/altnode/node-$ver-linux-x86/bin/node" "$HOME/bin/node"
-        rm -r "/tmp/altnode/node-$ver-linux-x86"
+        set -l tarball "$HOME/.altnode/cache/$ver.tar.gz"
+        set -l tmpdir "/tmp/altnode/node-$ver-linux-x86"
+
+        if test -e "$tarball"
+          tar -zxvf "$tarball" -C "/tmp/altnode" "node-$ver-linux-x86/bin/node"
+          mv "$tmpdir/bin/node" "$HOME/bin/node"
+          rm -r "$tmpdir"
+        else
+          echo "Node version '$ver' not found locally, fetch it first"
+        end
       case '*'
         echo "Command '$cmd' not supported, use no args to print help"
     end
@@ -28,7 +39,7 @@ function altnode
     echo "Usage: altnode {COMMAND}"
     echo -e '\tlocal: List local version'
     echo -e '\tremote: List remote version'
-    echo -e '\tclear: Remove altnode binary, use system binary'
+    echo -e '\treset: Remove altnode binary, use system binary'
     echo -e '\tfetch {VERSION}: Download specified version'
     echo -e '\tuse {VERSION}: Use specified version'
   end
