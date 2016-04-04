a658526f2e348113076a79dd2d46c9e29f24cd84
Oliver Caldwell <ollie@qubit.com>
2016-04-04 10:17:47 +0100

    Only allow one instance of i3lock

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-i3lock -i ~/.i3/backgrounds/congruent_outline.png --tiling && sleep 1
+if [[ -z "$(pidof i3lock)" ]]; then
+  i3lock -i ~/.i3/backgrounds/congruent_outline.png --tiling && sleep 1
+else
+  echo "i3lock is already running"
+fi
