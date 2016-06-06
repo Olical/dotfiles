2804c2d1e588ab24e96078d4b1d0782ab7acfaf8
Oliver Caldwell <olliec87@gmail.com>
2016-06-06 10:32:07 +0100

    Remove unused statements in update
    ARCH IS LOVE, ARCH IS LIFE

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,17 +1,8 @@
 function update
   cd ~/dotfiles; git pull; cd -
 
-  if [ (which yaourt) ]
-    yaourt -Syua --noconfirm
-    yaourt -C
-  else if [ (which pacman) ]
-    sudo pacman -Syu --noconfirm
-    sudo pacdiff
-  else if [ (which apt-get) ]
-    sudo apt-get update; and sudo apt-get upgrade
-  else if [ (which brew) ]
-    brew update; and brew upgrade
-  end
+  yaourt -Syua --noconfirm
+  yaourt -C
 
   for package in (npm -g outdated --parseable --depth=0 | grep -v "@git\|@linked" | cut -d: -f2)
     npm -g install "$package"
