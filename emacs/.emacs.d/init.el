34742ebb42f19ee6c78d2cce2e1c4f4ff061d140
Oliver Caldwell <olliec87@gmail.com>
2016-10-25 17:51:11 +0100

    Add paredit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -134,5 +134,28 @@
   (add-hook 'text-mode-hook 'flyspell-mode)
   (add-hook 'prog-mode-hook 'flyspell-prog-mode))
 
+(use-package paredit
+  :ensure t
+  :config
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook #'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode))
+
+(use-package evil-paredit
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'evil-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'evil-paredit-mode)
+  (add-hook 'ielm-mode-hook #'evil-paredit-mode)
+  (add-hook 'lisp-mode-hook #'evil-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'evil-paredit-mode)
+  (add-hook 'scheme-mode-hook #'evil-paredit-mode)
+  (add-hook 'clojure-mode-hook #'evil-paredit-mode))
+
 (provide 'init)
 ;;; init.el ends here
