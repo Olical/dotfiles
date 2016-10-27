d76a5a0d7f3bd7f66d69b25096d97013af44ae85
Oliver Caldwell <olliec87@gmail.com>
2016-10-27 20:22:06 +0100

    Set up [SPC and ]SPC new line binds

--- emacs/.emacs.d/pkgs/olical-evil.el
+++ emacs/.emacs.d/pkgs/olical-evil.el
@@ -7,5 +7,24 @@
 
 ;;; Code:
 
+(require 'evil)
+
+(defun olical-evil-new-line-above (count)
+  "Create new lines above your current one.  COUNT is the number of new lines."
+  (interactive "p")
+  (evil-open-above count)
+  (evil-force-normal-state)
+  (evil-next-line 1))
+
+(defun olical-evil-new-line-below (count)
+  "Create new lines below your current one.  COUNT is the number of new lines."
+  (interactive "p")
+  (evil-open-below count)
+  (evil-force-normal-state)
+  (evil-previous-line 1))
+
+(define-key evil-normal-state-map (kbd "[ SPC") 'olical-evil-new-line-above)
+(define-key evil-normal-state-map (kbd "] SPC") 'olical-evil-new-line-below)
+
 (provide 'olical-evil)
 ;;; olical-evil.el ends here
