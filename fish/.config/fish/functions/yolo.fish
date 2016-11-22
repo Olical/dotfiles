483b64b813f6c4384b4d75c3ec789c241f281ecf
Oliver Caldwell <olliec87@gmail.com>
2016-11-22 14:12:39 +0000

    update -> yolo

--- fish/.config/fish/functions/yolo.fish
+++ fish/.config/fish/functions/yolo.fish
@@ -1,4 +1,7 @@
-function update
+function yolo
+  # Early sudo because it's needed later.
+  sudo echo "Updating the entire bloody universe."
+
   cd ~/dotfiles; git pull; cd -
 
   yaourt -Syua --noconfirm
