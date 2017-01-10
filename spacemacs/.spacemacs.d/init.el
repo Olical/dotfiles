29e0197d6ea6e227cebffb51f613710d00c15420
Oliver Caldwell <olliec87@gmail.com>
2017-01-10 09:20:51 +0000

    Move git gutter to the left

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -311,6 +311,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq git-gutter-fr+-side 'left-fringe)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
