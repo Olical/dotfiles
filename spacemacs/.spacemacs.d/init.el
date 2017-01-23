b7ec73b15cd9d49112edb8fc56945aa23a4c646b
Oliver Caldwell <olliec87@gmail.com>
2017-01-23 15:50:54 +0000

    Automatically connect to figwheel on cljs jack in

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -312,6 +312,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
