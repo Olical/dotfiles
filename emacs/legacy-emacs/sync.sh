f3f37ee006c1401cf8eb9be4199185466cc90db8
Oliver Caldwell <olliec87@gmail.com>
2017-01-24 10:52:46 +0000

    Remove legacy emacs directory

--- emacs/legacy-emacs/sync.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-emacs --user `whoami` --batch -l ~/.emacs.d/package-bootstrap.el -f package-refresh-contents
-emacs --user `whoami` --batch -l ~/.emacs.d/init.el --eval "(paradox-upgrade-packages)"
