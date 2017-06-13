476d022cccfc84f4528019dfb67890d5b61747e8
Oliver Caldwell <olliec87@gmail.com>
2017-06-13 09:19:01 +0100

    Fix locker. Runs in the background but blocks on compton

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ -z "$(pidof i3lock)" ]]; then
-  killall compton
-  i3lock -n -i ~/.i3/backgrounds/congruent_outline.png --tiling
-  compton -b --config ~/.i3/compton.conf
+  {
+      killall compton
+      i3lock -n -i ~/.i3/backgrounds/congruent_outline.png --tiling
+      compton -b --config ~/.i3/compton.conf
+  } &
 else
   echo "i3lock is already running"
 fi
