3f00092475b1b6d3c5b993caec64a988dd4616f7
Oliver Caldwell <olliec87@gmail.com>
2017-06-18 17:13:40 +0100

    Set the geiser repl to racket

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -330,7 +330,7 @@ you should place your code here."
    web-mode-css-indent-offset 2
    web-mode-code-indent-offset 2
    web-mode-attr-indent-offset 2)
-  (setq geiser-active-implementations '(mit))
+  (setq geiser-active-implementations '(racket))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
