c8bc39efa5a972395e4cda628b6d496cd61efa96
Oliver Caldwell <olliec87@gmail.com>
2017-06-27 15:25:30 +0100

    Add cloverage

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-cljfmt "0.5.6"]
-                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]]}}
+                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
+                  [lein-cloverage "1.0.9"]]}}
