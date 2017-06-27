8d32ced1c2020b7bf8828fd706f35d2ee88ae266
Oliver Caldwell <olliec87@gmail.com>
2017-06-27 14:57:52 +0100

    kibit and cljfmt require different clojure versions
    Exclude one.

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-cljfmt "0.5.6"]
-                  [lein-kibit "0.1.3"]]}}
+                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]]}}
