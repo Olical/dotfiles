c23981882d7f1e94bc76b4f38f5d95fa5c1b36bd
Oliver Caldwell <olliec87@gmail.com>
2017-08-02 14:12:15 +0100

    Add xmodmap to packages and update docker for new work

--- i3/.i3/docker.sh
+++ i3/.i3/docker.sh
@@ -7,11 +7,11 @@ xrandr > /dev/null
 xrandr | grep "$EXT disconnected" > /dev/null
 
 if [ $? -eq 0 ]; then
-  xrandr --output $IN --primary --auto --output $EXT --off
+  xrandr --output $IN --primary --mode 1920x1080 --output $EXT --off
   xmodmap -e "pointer = 1 2 3" 2> /dev/null
   echo "-> Laptop mode"
 else
-  xrandr --output $IN --auto --output $EXT --primary --auto --left-of $IN
+  xrandr --output $IN --mode 1920x1080 --output $EXT --primary --auto --right-of $IN
   xmodmap -e "pointer = 3 2 1" 2> /dev/null
   echo "-> Desktop mode"
 fi
