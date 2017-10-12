41c32a9b6306c6b4eeab2864ff78247758c84b5d
Oliver Caldwell <olliec87@gmail.com>
2017-10-12 13:29:32 +0100

    Move helm-bookmark inclusion
    My emacs is now broken... yay!

--- spacemacs/.spacemacs.d/layers/olical-lisp/config.el
+++ spacemacs/.spacemacs.d/layers/olical-lisp/config.el
@@ -2,4 +2,4 @@
 (setq cider-cljs-lein-repl
       "(do (require '[cljs.repl :as repl] '[cljs.repl.node :as node])
            (compile 'cljs.repl.node)
-           (cemerick.piggieback/cljs-repl (cljs.repl.node/repl-env)))")15
+           (cemerick.piggieback/cljs-repl (cljs.repl.node/repl-env)))")
