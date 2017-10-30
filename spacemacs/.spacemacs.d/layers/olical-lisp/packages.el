32d7a709821c5f5eeb4a9a5792633d9c9903e3f2
Oliver Caldwell <olliec87@gmail.com>
2017-10-30 15:38:30 +0000

    Remove parinfer, forces me to use lispy more

--- spacemacs/.spacemacs.d/layers/olical-lisp/packages.el
+++ spacemacs/.spacemacs.d/layers/olical-lisp/packages.el
@@ -1,24 +1,5 @@
 (defconst olical-lisp-packages
-  '(lispy
-    parinfer))
+  '(lispy))
 
 (defun olical-lisp/init-lispy ()
   (use-package lispy))
-
-(defun olical-lisp/init-parinfer ()
-  (use-package parinfer
-    :init
-    (progn
-      (setq-default parinfer-lighters '(" infer" . " (infer)"))
-      (setq-default parinfer-extensions
-                    '(defaults
-                       pretty-parens
-                       evil
-                       lispy
-                       smart-tab
-                       smart-yank))
-      (add-hook 'clojure-mode-hook #'parinfer-mode)
-      (add-hook 'racket-mode-hook #'parinfer-mode)
-      (add-hook 'scheme-mode-hook #'parinfer-mode)
-      (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-      (spacemacs/set-leader-keys-for-minor-mode 'parinfer-mode "," 'parinfer-toggle-mode))))
