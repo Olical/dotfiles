cb6c6abad8cd5290ba6b16e9c306a0f92ace34d6
Oliver Caldwell <olliec87@gmail.com>
2018-02-21 14:54:25 +0000

    Upgrade eastwood

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -2,4 +2,4 @@
                   [lein-cljfmt "0.5.6"]
                   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
                   [lein-cloverage "1.0.9"]
-                  [jonase/eastwood "0.2.4"]]}}
+                  [jonase/eastwood "0.2.5"]]}}
