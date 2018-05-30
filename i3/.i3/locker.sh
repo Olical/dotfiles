cfb4039ca2694e449757d3fbd76878b4fd8c5b37
Oliver Caldwell <olliec87@gmail.com>
2018-05-30 09:02:34 +0100

    Don't disable compton on lock now

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$(pidof i3lock)" ]]; then
-  {
-      killall compton
-      i3lock -n -i ~/.i3/backgrounds/congruent_outline.png --tiling
-      compton -b --config ~/.i3/compton.conf
-  } &
+  i3lock -n -i ~/.i3/backgrounds/congruent_outline.png --tiling &
 else
   echo "i3lock is already running"
 fi
