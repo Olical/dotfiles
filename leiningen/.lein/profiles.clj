d000284dcaf1b794ff970aa343437ce92c1dfc70
Oliver Caldwell <ollie@oli.me.uk>
2019-03-28 10:31:29 +0000

    Remove lein config

--- leiningen/.lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:plugins [[lein-ancient "0.6.10"]
-                  [lein-cljfmt "0.5.6"]
-                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-                  [lein-cloverage "1.0.9"]
-                  [jonase/eastwood "0.2.5"]]}}
