757c946db8367767923914e66cef8bbb0e25e2de
Oliver Caldwell <ollie@oli.me.uk>
2019-07-09 14:53:46 +0100

    For some reason my mode indicator was bold

--- fish/.config/fish/functions/fish_mode_prompt.fish
+++ fish/.config/fish/functions/fish_mode_prompt.fish
@@ -2,16 +2,16 @@ function fish_mode_prompt --description 'Displays the current mode'
   if test "$fish_key_bindings" = "fish_vi_key_bindings"
     switch $fish_bind_mode
       case default
-        set_color --bold red
+        set_color red
         echo 🅽
       case insert
-        set_color --bold green
+        set_color green
         echo 🅸
       case replace_one
-        set_color --bold green
+        set_color green
         echo 🆁
       case visual
-        set_color --bold brmagenta
+        set_color brmagenta
         echo 🆅
     end
     set_color normal
