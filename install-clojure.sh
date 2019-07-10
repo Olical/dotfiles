1fdbaca83ada5656d509d88254391fd4b4763a19
Oliver Caldwell <ollie@oli.me.uk>
2019-07-10 10:46:21 +0100

    Updating some Clojure tools

--- install-clojure.sh
+++ install-clojure.sh
@@ -4,10 +4,8 @@ set -xe
 
 cd /tmp
 
-curl -O https://download.clojure.org/install/linux-install-1.10.0.442.sh
-chmod +x linux-install-1.10.0.442.sh
-sudo ./linux-install-1.10.0.442.sh
+VERSION=1.10.1.462
 
-curl -sO https://raw.githubusercontent.com/borkdude/clj-kondo/master/script/install-clj-kondo
-chmod +x install-clj-kondo
-sudo ./install-clj-kondo
+curl -O https://download.clojure.org/install/linux-install-$VERSION.sh
+chmod +x linux-install-$VERSION.sh
+sudo ./linux-install-$VERSION.sh
