0825cb017ab7e61e0f32c92c73f4cf14b34e6b7c
Oliver Caldwell <ollie@oli.me.uk>
2019-08-12 15:15:03 +0100

    Change locker background to sloth too

--- i3/.i3/locker.sh
+++ i3/.i3/locker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$(pidof i3lock)" ]]; then
-  i3lock -n -i ~/.i3/backgrounds/list.png --tiling &
+  i3lock -n -i ~/.i3/backgrounds/sloth.png --tiling &
 else
   echo "i3lock is already running"
 fi
