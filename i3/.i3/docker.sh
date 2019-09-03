82a2c9b701c2f923a19e9eca2dfd5cfc1b19d445
Oliver Caldwell <ollie@oli.me.uk>
2019-09-03 14:01:11 +0100

    Remove keymap call and set higher res for ext

--- i3/.i3/docker.sh
+++ i3/.i3/docker.sh
@@ -3,9 +3,11 @@
 if [ "$hostname" == "ollie-hp" ]; then
   IN="eDP-1"
   EXT="DP-2"
+  EXT_RES=2560x1440
 else
   IN="eDP1"
   EXT="DP1"
+  EXT_RES=1920x1080
 fi
 
 xrandr > /dev/null
@@ -16,7 +18,7 @@ if [ $? -eq 0 ]; then
   xmodmap -e "pointer = 1 2 3" 2> /dev/null
   echo "-> Laptop mode"
 else
-  xrandr --output $IN --mode 1920x1080 --output $EXT --primary --mode 1920x1080 --right-of $IN
+  xrandr --output $IN --mode 1920x1080 --output $EXT --primary --mode $EXT_RES --right-of $IN
   xmodmap -e "pointer = 3 2 1" 2> /dev/null
   echo "-> Desktop mode"
 fi
