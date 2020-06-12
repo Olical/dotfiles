072a82fb987cb6d4fa757611912b2aa9ecd71b44
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-06-12 10:14:48 +0000

    Load fzf fish bindings from nix

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,10 @@
 function fish_user_key_bindings
   fish_vi_key_bindings
 
+  if command -s fzf-share >/dev/null
+    source (fzf-share)/key-bindings.fish
+  end
+
   if type -q fzf_key_bindings
     fzf_key_bindings
   end
