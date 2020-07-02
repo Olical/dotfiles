51a6d8fc8f98437d31121d226eba4452e3a3823a
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-07-02 16:38:58 +0100

    Possibly better sync script

--- neovim/.config/nvim/sync.sh
+++ neovim/.config/nvim/sync.sh
@@ -4,10 +4,12 @@ CONFIG=$HOME/.config/nvim
 
 mkdir -p $CONFIG/undo $CONFIG/spell $CONFIG/autoload
 
-if [ ! -f $CONFIG/autoload/plug.vim ]; then
-    curl -fLo $CONFIG/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -f $CONFIG/autoload/plug.vim ]; then
+  rm -rf $CONFIG/lua
+  nvim +"PlugSnapshot! $CONFIG/snapshot.vim" +PlugUpgrade +PlugClean! +PlugUpdate +qa
+  nvim +UpdateRemotePlugins +qa
+else
+  curl -fLo $CONFIG/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  nvim +PlugInstall
+  nvim +UpdateRemotePlugins +qa
 fi
-
-rm -rf $CONFIG/lua
-nvim +"PlugSnapshot! $CONFIG/snapshot.vim" +PlugUpgrade +PlugClean! +PlugUpdate +qa
-nvim +UpdateRemotePlugins +qa
