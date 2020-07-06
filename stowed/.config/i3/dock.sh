0bbe5e6c3680e28579c9a21c0310d239b9862941
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-07-06 20:58:45 +0100

    Set primary monitor

--- stowed/.config/i3/dock.sh
+++ stowed/.config/i3/dock.sh
@@ -2,9 +2,9 @@
 
 if xrandr | grep "DP-1-3 connected"; then
   xrandr --output eDP-1 --mode 1920x1080
-  xrandr --output DP-1-3 --mode 1920x1080 --left-of eDP-1
+  xrandr --output DP-1-3 --mode 1920x1080 --left-of eDP-1 --primary
 else
-  xrandr --output eDP-1 --mode 1920x1080
+  xrandr --output eDP-1 --mode 1920x1080 --primary
   xrandr --output DP-1-3 --off
 fi
 
