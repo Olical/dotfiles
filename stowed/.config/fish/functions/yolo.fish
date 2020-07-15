90a22cb863483dc92d10ce561ce16823da53787d
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-07-15 11:45:00 +0100

    Lighter yolo if guest

--- stowed/.config/fish/functions/yolo.fish
+++ stowed/.config/fish/functions/yolo.fish
@@ -4,14 +4,18 @@ function yolo
 
   cd ~/.config/nixpkgs; git pull --no-rebase; cd -
 
-  sudo nix-channel --update
-  sudo nixos-rebuild switch
+  if ! set -q DOTFILES_GUEST
+    sudo nix-channel --update
+    sudo nixos-rebuild switch
+  end
 
   nix-channel --update
   home-manager switch
 
-  sudo nix-env --delete-generations 14d
-  sudo nix-store --gc
+  if ! set -q DOTFILES_GUEST
+    sudo nix-env --delete-generations 14d
+    sudo nix-store --gc
+  end
 
   ~/.config/nvim/sync.sh
 end
