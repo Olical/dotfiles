56c2a73749e39ee41126666518912388d2e7839d
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-08-07 09:25:33 +0000

    Tweak my update script for pairing host

--- stowed/.config/fish/functions/yolo.fish
+++ stowed/.config/fish/functions/yolo.fish
@@ -4,17 +4,21 @@ function yolo
 
   cd ~/.config/nixpkgs; git pull --no-rebase; cd -
 
-  if ! set -q DOTFILES_GUEST
+  if ! set -q DOTFILES_GUEST; and ! set -q DOTFILES_JUST_NIX
     sudo nix-channel --update
     sudo nixos-rebuild switch
   end
 
   nix-channel --update
   home-manager switch
+  nix-env --delete-generations 14d
 
   if ! set -q DOTFILES_GUEST
-    sudo nix-env --delete-generations 14d
-    sudo nix-store --gc
+    if ! set -q DOTFILES_JUST_NIX
+      sudo nix-env --delete-generations 14d
+    end
+
+    nix-store --gc
   end
 
   ~/.config/nvim/sync.sh
