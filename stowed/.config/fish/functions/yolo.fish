7f1336ab486603fae6d6dec75c5f9975e6834d63
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-10-07 15:01:35 +0000

    Tidy up yolo, no longer has to worry about non-nixos

--- stowed/.config/fish/functions/yolo.fish
+++ stowed/.config/fish/functions/yolo.fish
@@ -4,7 +4,7 @@ function yolo
 
   cd ~/.config/nixpkgs; git pull --no-rebase; cd -
 
-  if ! set -q DOTFILES_GUEST; and ! set -q DOTFILES_JUST_NIX
+  if ! set -q DOTFILES_GUEST
     sudo nix-channel --update
     sudo nixos-rebuild switch
   end
@@ -14,18 +14,8 @@ function yolo
   nix-env --delete-generations 14d
 
   if ! set -q DOTFILES_GUEST
-    if ! set -q DOTFILES_JUST_NIX
-      sudo nix-env --delete-generations 14d
-    end
-
+    sudo nix-env --delete-generations 14d
     nix-store --gc
-
-    if type -q apt-get
-      sudo apt-get update
-      sudo apt-get upgrade -y
-      sudo apt-get autoclean -y
-      sudo apt-get autoremove -y
-    end
   end
 
   ~/.config/nvim/sync.sh
