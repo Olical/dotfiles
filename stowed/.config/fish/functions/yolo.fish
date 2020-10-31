8499a4b5f06c6840e0567c79ff4693636bf8a44d
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-10-31 16:08:52 +0000

    Update yolo with autostash

--- stowed/.config/fish/functions/yolo.fish
+++ stowed/.config/fish/functions/yolo.fish
@@ -2,7 +2,7 @@ function yolo
   # Early sudo because it's needed later.
   sudo echo "Updating the entire bloody universe."
 
-  cd ~/.config/nixpkgs; git pull --no-rebase; cd -
+  cd ~/.config/nixpkgs; git pull --rebase --autostash; cd -
 
   if ! set -q DOTFILES_GUEST
     sudo nix-channel --update
