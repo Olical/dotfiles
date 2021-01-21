26bae48b0559c75491b58165230d49c26ff09fbd
Oliver Caldwell <oliver.caldwell@viasat.com>
2021-01-21 14:38:16 +0000

    Correct path for npm bin

--- stowed/.config/fish/config.fish
+++ stowed/.config/fish/config.fish
@@ -1,7 +1,7 @@
 set -gx NPM_PACKAGES "$HOME/.npm-packages"
 set -gx JANET_PATH ~/.cache/janet
 set -gx NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-set -gx PATH ~/bin ~/npm-packages/bin ~/.cargo/bin ~/.luarocks/bin $PATH
+set -gx PATH ~/bin $NPM_PACKAGES/bin ~/.cargo/bin ~/.luarocks/bin $PATH
 
 mkdir -p ~/bin $NPM_PACKAGES/bin ~/.cargo/bin ~/.luarocks/bin $JANET_PATH
 
