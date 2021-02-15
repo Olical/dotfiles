39f21cad85c328f446557918574ffcd2c5d3662c
Oliver Caldwell <oliver.caldwell@viasat.com>
2021-02-15 14:24:12 +0000

    Better Hy

--- home.nix
+++ home.nix
@@ -4,7 +4,19 @@ let
   dag = config.lib.dag;
   unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {};
   thunar = pkgs.xfce.thunar.override { thunarPlugins = [pkgs.xfce.thunar-archive-plugin]; };
-  python = unstable.python3.withPackages (ps: with ps; [ pynvim ]);
+  hy = unstable.python3.pkgs.buildPythonPackage rec {
+    pname = "hy";
+    version = "0.20.0";
+    src = unstable.python3.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "1b72863754fb57e2dd275a9775bf621cb50a565e76733a2e74e9954e7fbb060e";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with unstable.python3Packages; [
+      astor colorama funcparserlib rply
+    ];
+  };
+  python = unstable.python3.withPackages (ps: with ps; [ pynvim hy ]);
   dwarf-fortress = unstable.dwarf-fortress-packages.dwarf-fortress-full.override {
     enableIntro = false;
     enableSound = false;
@@ -36,7 +48,6 @@ in
     gnumake
     htop
     httpie
-    hy
     joker
     killall
     lazygit
