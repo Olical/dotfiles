bea32eb03c27cf24792571db1595d6c5bfda8786
Oliver Caldwell <oliver.caldwell@viasat.com>
2021-03-18 15:48:12 +0000

    Better documentation of which monitor is which

--- stowed/.config/i3/dock.sh
+++ stowed/.config/i3/dock.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP-1 --off
-xrandr --output DP-1-3 --off
-xrandr --output DP-1-1 --off
+xrandr --output eDP-1 --off # Built in
+xrandr --output DP-1-3 --off # BenQ
+xrandr --output DP-1-1 --off # Samsung
 
 if xrandr | grep "DP-1-3 connected"; then
-  xrandr --output DP-1-3 --mode 1920x1080 --primary # BENQ
-  xrandr --output DP-1-1 --mode 1920x1080 --right-of DP-1-3 # SAMSUNG
+  xrandr --output DP-1-3 --mode 1920x1080 --primary
+  xrandr --output DP-1-1 --mode 1920x1080 --right-of DP-1-3
 else
   xrandr --output eDP-1 --mode 1920x1080 --primary
 fi
