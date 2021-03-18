e1f9c2a3a1a2a272d79974953dab0d2c1e92b03f
Oliver Caldwell <oliver.caldwell@viasat.com>
2021-03-18 14:37:00 +0000

    Update dock script, my laptop is now under my desk

--- stowed/.config/i3/dock.sh
+++ stowed/.config/i3/dock.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+xrandr --output eDP-1 --off
+xrandr --output DP-1-3 --off
+xrandr --output DP-1-1 --off
+
 if xrandr | grep "DP-1-3 connected"; then
-  xrandr --output eDP-1 --mode 1920x1080
-  xrandr --output DP-1-3 --mode 1920x1080 --left-of eDP-1 --primary
+  xrandr --output DP-1-3 --mode 1920x1080 --primary # BENQ
+  xrandr --output DP-1-1 --mode 1920x1080 --right-of DP-1-3 # SAMSUNG
 else
   xrandr --output eDP-1 --mode 1920x1080 --primary
-  xrandr --output DP-1-3 --off
 fi
 
 feh --bg-scale ~/.config/i3/background-image
