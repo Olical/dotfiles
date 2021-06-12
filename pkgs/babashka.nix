b86ecf285e9eb533e9e2b5953d76f32614f50d36
Oliver Caldwell <ollie@oli.me.uk>
2021-06-12 16:33:57 +0100

    Update babashka

--- pkgs/babashka.nix
+++ pkgs/babashka.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "babashka";
-  version = "0.4.1";
+  version = "0.4.5";
 
   src = fetchurl {
     url = "https://github.com/borkdude/babashka/releases/download/v${version}/babashka-${version}-linux-amd64.tar.gz";
-    sha256 = "1wsv5fq8a4692q327nxvh9ca1ajzsdkl0v2v2vh5a5lzicarzc9w";
+    sha256 = "1qd5zjasf0swwswxh5c5mqia6pixysnh81pnawjmsibhzr9wpk2k";
   };
 
   nativeBuildInputs = [
