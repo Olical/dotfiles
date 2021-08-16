be6350816bf4a336748eccf58d745546bdf131a3
Oliver Caldwell <oliver.caldwell@viasat.com>
2021-08-16 17:19:07 +0100

    Better sync, again!

--- stowed/.config/nvim/sync.sh
+++ stowed/.config/nvim/sync.sh
@@ -3,4 +3,4 @@
 CONFIG=$HOME/.config/nvim
 mkdir -p $CONFIG/undo $CONFIG/spell $CONFIG/autoload
 rm -rf $CONFIG/lua
-nvim +TSUpdateSync +"au User PackerComplete qa" +PackerSync
+nvim +"au User PackerComplete qa" +PackerSync +"silent TSUpdateSync"
