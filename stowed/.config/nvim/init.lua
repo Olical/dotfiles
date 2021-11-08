84230e6f6380b3737d21ecf4527f9310822791cb
Arthur <arthur.fayzullin@gmail.com>
2021-11-08 15:46:03 +0100

    Update init.lua
    Remove extra repo

--- stowed/.config/nvim/init.lua
+++ stowed/.config/nvim/init.lua
@@ -10,7 +10,7 @@ local fmt = string.format
 
 function ensure (user, repo)
   -- Ensures a given github.com/USER/REPO is cloned in the pack/packer/start directory.
-  local install_path = fmt("%s/packer/start/%s", pack_path, repo, repo)
+  local install_path = fmt("%s/packer/start/%s", pack_path, repo)
   if fn.empty(fn.glob(install_path)) > 0 then
     execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
     execute(fmt("packadd %s", repo))
