cfb7718934e73fe724290c93f24ccf4dcf85fae7
Oliver Caldwell <ollie@oli.me.uk>
2022-02-12 14:51:44 +0000

    Install fish from brew, not apt

--- script/install-packages.sh
+++ script/install-packages.sh
@@ -6,13 +6,13 @@ sudo apt update
 
 sudo apt install -y \
   kitty fzf cowsay curl entr \
-  fish httpie htop nodejs python3 stow tree \
+  httpie htop nodejs python3 stow tree \
   luajit asciinema make ripgrep \
   fonts-firacode fonts-noto fonts-noto-cjk fonts-noto-color-emoji \
   libavcodec-extra ubuntu-restricted-extras
 
 sudo apt install -y build-essential procps curl file git
 brew install \
-  neovim clojure clojure-lsp joker lazygit \
+  fish neovim clojure clojure-lsp joker lazygit \
   lua-language-server xmlformat bitwarden-cli \
   borkdude/brew/babashka candid82/brew/joker
