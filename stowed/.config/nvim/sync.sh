34829b1647ea25617a3f465d95fcc9910cce57ec
Oliver Caldwell <ollie@oli.me.uk>
2022-04-18 17:02:29 +0100

    Sync tree-sitter modules each time

--- stowed/.config/nvim/sync.sh
+++ stowed/.config/nvim/sync.sh
@@ -4,4 +4,4 @@ CONFIG=$HOME/.config/nvim
 mkdir -p $CONFIG/undo $CONFIG/spell $CONFIG/autoload
 rm -rf $CONFIG/lua
 nvim +qa
-nvim +"au User PackerComplete qa" +PackerSync
+nvim +"au User PackerComplete qa" +PackerSync +TSUpdateSync
