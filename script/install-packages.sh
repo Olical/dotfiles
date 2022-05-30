ff3917bbf70a26a727d983ce954a49d5da27246e
Oliver Caldwell <ollie@oli.me.uk>
2022-05-30 12:03:10 +0100

    Support EndeavourOS (Arch) instead of Regolith (Ubuntu)

--- script/install-packages.sh
+++ script/install-packages.sh
@@ -2,17 +2,7 @@
 
 set -xe
 
-sudo apt update
-
-sudo apt install -y \
-  kitty fzf cowsay curl entr fish \
-  httpie htop nodejs python3 stow tree \
-  lua asciinema make ripgrep \
-  fonts-firacode fonts-noto fonts-noto-cjk fonts-noto-color-emoji \
-  libavcodec-extra ubuntu-restricted-extras
-
-sudo apt install -y build-essential procps curl file git
-brew install \
-  fish neovim clojure clojure-lsp joker lazygit \
-  lua-language-server xmlformat bitwarden-cli \
-  borkdude/brew/babashka candid82/brew/joker
+yay -S \
+  kitty fzf cowsay entr fish httpie htop nodejs stow \
+  asciinema ripgrep clojure clojure-lsp lazygit tree \
+  lua-language-server bitwarden-cli babashka lua
--- stowed/.config/fish/functions/sync.fish
+++ stowed/.config/fish/functions/sync.fish
@@ -2,11 +2,7 @@ function sync
   # Early sudo because it's needed later.
   sudo echo "Updating everything!"
 
-  sudo apt update
-  sudo apt upgrade
-
-  brew update
-  brew upgrade
+  yay
 
   cd ~/.config/nvim
   git pull --rebase --autostash
