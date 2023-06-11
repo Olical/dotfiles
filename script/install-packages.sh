b87e0bbac09c444b385b2ef5b7be76abf2ed1fc1
Oliver Caldwell <oliver.caldwell@viasat.com>
2023-06-11 21:01:02 +0100

    Add fira code nerd font

--- script/install-packages.sh
+++ script/install-packages.sh
@@ -3,11 +3,8 @@
 set -xe
 
 yay -S --needed \
-  kitty fzf cowsay entr fish httpie htop npm nodejs \
-  asciinema ripgrep clojure clojure-lsp lazygit tree \
-  lua-language-server bash-language-server bitwarden-cli \
-  babashka lua xclip ttf-fira-code ttf-dejavu-emojiless \
-  noto-fonts-emoji gnome-keyring topgrade neovim leiningen \
-  difftastic direnv
-
-npm install -g typescript-language-server
+	kitty fzf cowsay entr fish httpie htop npm nodejs \
+	asciinema ripgrep clojure lazygit tree bitwarden-cli \
+	babashka lua xclip ttf-fira-code ttf-dejavu-emojiless \
+	noto-fonts-emoji gnome-keyring topgrade neovim leiningen \
+	difftastic direnv ttf-firacode-nerd neovide
