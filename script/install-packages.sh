136293b75b19c6a4ebb0223123ce956835636d00
Oliver Caldwell <ollie@oli.me.uk>
2023-07-03 11:40:33 +0100

    LTS node

--- script/install-packages.sh
+++ script/install-packages.sh
@@ -3,7 +3,7 @@
 set -xe
 
 yay -S --needed \
-	kitty fzf cowsay entr fish httpie htop npm nodejs \
+	kitty fzf cowsay entr fish httpie htop npm nodejs-lts-hydrogen \
 	asciinema ripgrep clojure lazygit tree bitwarden-cli \
 	babashka lua xclip ttf-fira-code ttf-dejavu-emojiless \
 	noto-fonts-emoji gnome-keyring topgrade neovim leiningen \
