64c06476a84f6aa4464f8876f5f1cd35adc61d0a
Oliver Caldwell <ollie@oli.me.uk>
2024-07-26 11:56:47 +0100

    Don't topgrade guix, it takes forever

--- stowed/.config/fish/functions/sync.fish
+++ stowed/.config/fish/functions/sync.fish
@@ -2,7 +2,7 @@ function sync
   # Early sudo because it's needed later.
   sudo echo "Updating everything!"
 
-  topgrade --disable git_repos containers vim
+  topgrade --disable git_repos containers vim guix
 
   cd ~/repos/Olical/dotfiles
   git checkout -- stowed/.config/nvim/lazy-lock.json
