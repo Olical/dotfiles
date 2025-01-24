b204a316a740996f5a269e9dd9a424a61bfb1367
Oliver Caldwell <ollie@oli.me.uk>
2025-01-24 12:59:42 +0000

    No just i mode lol

--- stowed/.config/nvim/init.lua
+++ stowed/.config/nvim/init.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 vim.keymap.set("n", "\\", ",")
-vim.keymap.set({"i", "v"}, "jk", "<esc>")
+vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<leader>q", "<CMD>:quit<CR>", {desc = ":quit"})
 vim.keymap.set("n", "\\", "<CMD>:split<CR>", {desc = ":split"})
 vim.keymap.set("n", "|", "<CMD>:vsplit<CR>", {desc = ":vsplit"})
