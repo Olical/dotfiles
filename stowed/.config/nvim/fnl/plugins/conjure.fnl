f90046af0d5fa847248868d2896d7eb63c9aa8d2
Oliver Caldwell <ollie@oli.me.uk>
2025-01-25 17:48:01 +0000

    Add conjure to stop things from breaking

---
+++ stowed/.config/nvim/fnl/plugins/conjure.fnl
@@ -0,0 +1,6 @@
+(import-macros {: tx} :config.macros)
+
+(set vim.g.conjure#filetype#fennel "conjure.client.fennel.nfnl")
+
+(tx "Olical/conjure"
+  {:event "VeryLazy"})
---
+++ stowed/.config/nvim/lua/plugins/conjure.lua
@@ -0,0 +1,3 @@
+-- [nfnl] Compiled from fnl/plugins/conjure.fnl by https://github.com/Olical/nfnl, do not edit.
+vim.g["conjure#filetype#fennel"] = "conjure.client.fennel.nfnl"
+return {"Olical/conjure", event = "VeryLazy"}
