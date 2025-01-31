5e4776de435a45f51fc1beef12af48cad62d3299
Oliver Caldwell <ollie@oli.me.uk>
2025-01-31 00:00:58 +0000

    Use yay-bin

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -3,7 +3,8 @@
 set -xe
 
 if ! command -v yay &>/dev/null; then
-	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf ./yay
+	# sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf ./yay
+	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf ./yay-bin
 fi
 
 yay -S --needed stow git
