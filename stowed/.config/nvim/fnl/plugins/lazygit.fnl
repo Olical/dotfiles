9ebbd42727793178bf4396ce00111e7137476f5f
Oliver Caldwell <ollie@oli.me.uk>
2025-02-03 11:43:20 +0000

    Add lazygit

---
+++ stowed/.config/nvim/fnl/plugins/lazygit.fnl
@@ -0,0 +1,7 @@
+(import-macros {: tx} :config.macros)
+
+(tx "kdheepak/lazygit.nvim"
+  {:dependencies ["nvim-lua/plenary.nvim"]
+   :lazy true
+   :cmd ["LazyGit" "LazyGitConfig" "LazyGitCurrentFile" "LazyGitFilter" "LazyGitFilterCurrentFile"]
+   :keys [(tx "<leader>g" "<CMD>LazyGit<CR>" {:desc "LazyGit"})]})
---
+++ stowed/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/lazygit.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<leader>g", "<CMD>LazyGit<CR>", desc = "LazyGit"}}, lazy = true}
