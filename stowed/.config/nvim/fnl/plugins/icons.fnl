8a763f6746dd2fad8a55660e78275c4163e4cee4
Oliver Caldwell <ollie@oli.me.uk>
2025-03-12 16:29:10 +0000

    Add webdevicons

---
+++ stowed/.config/nvim/fnl/plugins/icons.fnl
@@ -0,0 +1,4 @@
+(import-macros {: tx} :config.macros)
+
+[(tx "nvim-tree/nvim-web-devicons" {:lazy false :opts {}})
+ (tx "echasnovski/mini.icons" {:lazy false :opts {}})]
---
+++ stowed/.config/nvim/lua/plugins/icons.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/icons.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"nvim-tree/nvim-web-devicons", lazy = false, opts = {}}, {"echasnovski/mini.icons", lazy = false, opts = {}}}
