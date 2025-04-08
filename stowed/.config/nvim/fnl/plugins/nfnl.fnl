d8302d13f9836609477ec89a5acabb4508b45a8e
Oliver Caldwell <ollie@oli.me.uk>
2025-04-08 18:52:40 +0100

    Use local dev nfnl

--- stowed/.config/nvim/fnl/plugins/nfnl.fnl
+++ stowed/.config/nvim/fnl/plugins/nfnl.fnl
@@ -1,3 +1,3 @@
 (import-macros {: tx} :config.macros)
 
-(tx "Olical/nfnl" {:ft "fennel"})
+(tx "Olical/nfnl" {:ft "fennel" :dev true})
--- stowed/.config/nvim/lua/plugins/nfnl.lua
+++ stowed/.config/nvim/lua/plugins/nfnl.lua
@@ -1,2 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/nfnl.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"Olical/nfnl", ft = "fennel"}
+return {"Olical/nfnl", dev = true, ft = "fennel"}
