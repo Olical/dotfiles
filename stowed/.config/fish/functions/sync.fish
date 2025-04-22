209789ec3a00def0e590a1cb36743e4d379f1347
Oliver Caldwell <ollie@oli.me.uk>
2025-04-22 09:48:21 +0100

    Don't try to update uv

--- stowed/.config/fish/functions/sync.fish
+++ stowed/.config/fish/functions/sync.fish
@@ -2,7 +2,7 @@ function sync
   # Early sudo because it's needed later.
   sudo echo "Updating everything!"
 
-  topgrade --disable containers vim guix
+  topgrade --disable containers vim guix uv
 
   nvim --headless "+Lazy! sync" +TSUpdateSync -c 'autocmd User MasonUpdateAllComplete quitall' -c 'MasonUpdateAll'
 end
