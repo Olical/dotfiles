4158a7091d3b3dffc705fd621fc5eda666cce152
Oliver Caldwell <ollie@oli.me.uk>
2025-04-26 10:05:19 +0100

    Perform a git pull of dotfiles first in sync so we fail early

--- stowed/.config/fish/functions/sync.fish
+++ stowed/.config/fish/functions/sync.fish
@@ -2,6 +2,8 @@ function sync
   # Early sudo because it's needed later.
   sudo echo "Updating everything!"
 
+  (cd ~/repos/Olical/dotfiles; and git pull)
+
   topgrade --disable containers vim guix uv
 
   nvim --headless "+Lazy! sync" +TSUpdateSync -c 'autocmd User MasonUpdateAllComplete quitall' -c 'MasonUpdateAll'
