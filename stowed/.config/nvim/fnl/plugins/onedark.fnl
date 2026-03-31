7e2a566f88d175dd366ee7c8db7327eacab4cf97
Oliver Caldwell <ollie@oli.me.uk>
2026-03-31 16:03:55 +0100

    Swap onedark to monokai-pro

--- stowed/.config/nvim/fnl/plugins/onedark.fnl
+++
@@ -1,8 +0,0 @@
-(import-macros {: tx} :config.macros)
-
-(tx "navarasu/onedark.nvim"
-  {:opts {:style "darker"}
-   :config (fn [_ opts]
-	     (let [onedark (require :onedark)]
-	       (onedark.setup opts)
-	       (onedark.load)))})
--- stowed/.config/nvim/lua/plugins/onedark.lua
+++
@@ -1,7 +0,0 @@
--- [nfnl] fnl/plugins/onedark.fnl
-local function _1_(_, opts)
-  local onedark = require("onedark")
-  onedark.setup(opts)
-  return onedark.load()
-end
-return {"navarasu/onedark.nvim", config = _1_, opts = {style = "darker"}}
