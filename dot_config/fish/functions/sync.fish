387cfd00684ff05b2bdf900f642a002eb9ed7529
Oliver Caldwell <ollie@oli.me.uk>
2026-03-31 17:17:26 +0100

    Add chezmoi config with autoPush, update sync function

--- dot_config/fish/functions/sync.fish
+++ dot_config/fish/functions/sync.fish
@@ -1,12 +1,11 @@
 function sync
-  cd ~/repos/Olical/dotfiles
-
   # Early sudo because it's needed later.
   sudo echo "Updating everything!"
-
-  git pull
 
   topgrade --disable containers vim guix uv
 
   nvim --headless "+Lazy! sync" +TSUpdate -c 'autocmd User MasonUpdateAllComplete quitall' -c 'MasonUpdateAll'
+
+  # Pick up any changes (e.g. lazy-lock.json) and auto-commit/push.
+  chezmoi re-add
 end
