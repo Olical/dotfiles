3abd2842135079ee754ab71d7fdc39ebe961d323
Oliver Caldwell <ollie@oli.me.uk>
2026-04-10 15:09:29 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/dap.lua

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -3,7 +3,7 @@ local clojure_dap_path = "/home/olical/repos/Olical/clojure-dap"
 local function _1_()
   local dap = require("dap")
   dap.adapters.clojure = {type = "executable", command = "clojure", args = {"-Sdeps", ("{:deps {clojure-dap/clojure-dap {:local/root \"" .. clojure_dap_path .. "\"}}}"), "-X", "clojure-dap.main/run"}}
-  dap.configurations.clojure = {{"Attach to nREPL", request = "attach", type = "clojure"}}
+  dap.configurations.clojure = {{name = "Attach to nREPL", type = "clojure", request = "attach"}}
   return nil
 end
 local function _2_()
@@ -30,4 +30,25 @@ end
 local function _9_()
   return require("dap").terminate()
 end
-return {{"mfussenegger/nvim-dap", config = _1_, keys = {{"<leader>db", _2_, desc = "Toggle breakpoint"}, {"<leader>dc", _3_, desc = "Continue / Start debug"}, {"<leader>dn", _4_, desc = "Step over"}, {"<leader>di", _5_, desc = "Step into"}, {"<leader>do", _6_, desc = "Step out"}, {"<leader>dr", _7_, desc = "Open REPL"}, {"<leader>dl", _8_, desc = "Run last"}, {"<leader>dx", _9_, desc = "Terminate"}}}}
+local function _10_()
+  return require("dapui").toggle()
+end
+local function _11_()
+  local dapui = require("dapui")
+  local dap = require("dap")
+  dapui.setup()
+  local function _12_()
+    return dapui.open()
+  end
+  dap.listeners.after.event_initialized["dapui"] = _12_
+  local function _13_()
+    return dapui.close()
+  end
+  dap.listeners.before.event_terminated["dapui"] = _13_
+  local function _14_()
+    return dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui"] = _14_
+  return nil
+end
+return {{"mfussenegger/nvim-dap", config = _1_, keys = {{"<leader>db", _2_, desc = "Toggle breakpoint"}, {"<leader>dc", _3_, desc = "Continue / Start debug"}, {"<leader>dn", _4_, desc = "Step over"}, {"<leader>di", _5_, desc = "Step into"}, {"<leader>do", _6_, desc = "Step out"}, {"<leader>dr", _7_, desc = "Open REPL"}, {"<leader>dl", _8_, desc = "Run last"}, {"<leader>dx", _9_, desc = "Terminate"}, {"<leader>du", _10_, desc = "Toggle DAP UI"}}}, {"rcarriga/nvim-dap-ui", config = _11_, dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}}}
