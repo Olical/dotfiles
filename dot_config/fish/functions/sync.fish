be697179d88e74852692ff144885cf45573254ef
Oliver Caldwell <ollie@oli.me.uk>
2026-04-15 18:44:10 +0100

    Update .config/fish/functions/sync.fish

--- dot_config/fish/functions/sync.fish
+++ dot_config/fish/functions/sync.fish
@@ -1,11 +1,13 @@
 function sync
-  # Early sudo because it's needed later.
-  sudo echo "Updating everything!"
+    # Early sudo because it's needed later.
+    sudo echo "Updating everything!"
 
-  topgrade --disable containers vim guix uv
+    chezmoi update
 
-  nvim --headless -c 'lua require("config.sync")'
+    topgrade --disable containers vim guix uv
 
-  # Pick up any changes (e.g. lazy-lock.json) and auto-commit/push.
-  chezmoi re-add
+    nvim --headless -c 'lua require("config.sync")'
+
+    # Pick up any changes (e.g. lazy-lock.json) and auto-commit/push.
+    chezmoi re-add
 end
