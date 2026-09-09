62ae199569964d0aa0095e2a30ab27bef0c2d6cb
Oliver Caldwell <ollie@oli.me.uk>
2026-09-09 10:14:33 +0100

    Update .config/fish/functions/sync.fish

--- dot_config/fish/functions/sync.fish
+++ dot_config/fish/functions/sync.fish
@@ -5,7 +5,15 @@ function sync
     chezmoi re-add
     chezmoi update
 
-    topgrade --disable containers vim guix uv
+    set -l disabled_steps containers vim guix uv
+
+    if command -q dnf
+        sudo dnf upgrade; or return
+        sudo dnf offline clean; or return
+        set -a disabled_steps system
+    end
+
+    topgrade --disable $disabled_steps
 
     nvim --headless -c 'lua require("config.sync")'
 
